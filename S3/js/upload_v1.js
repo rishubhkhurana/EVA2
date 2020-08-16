c4c264e61203122b53bd8f7b30387df5c1265745
rishubhkhurana <rishubhkhurana@gmail.com>
2020-08-16 18:59:17 +0530

    added 4 apis

--- S3/js/upload_v1.js
+++ S3/js/upload_v1.js
@@ -12,7 +12,7 @@ function uploadAndClassifyImageRes(){
         aync: true,
         crossDomain: true,
         method: 'POST',
-        url: 'https://xxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
+        url: 'https://xxxxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
         data: formData,
         processData: false,
         contentType: false,
@@ -42,7 +42,7 @@ function uploadAndClassifyImageMobile(){
         aync: true,
         crossDomain: true,
         method: 'POST',
-        url: 'https://xxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
+        url: 'https://xxxxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
         data: formData,
         processData: false,
         contentType: false,
@@ -72,6 +72,48 @@ function alignImage(){
         aync: true,
         crossDomain: true,
         method: 'POST',
+        url: 'https://xxxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
+        data: formData,
+        processData: false,
+        contentType: false,
+        mimeType: "multipart/form-data"
+    })
+    .done(function (response){
+        console.log(response);
+        document.getElementById('imageresult').src = response;
+    })
+    .fail(function(){
+        alert ("There was error while sending prediction request to mobile model.");
+    });
+};
+
+function swapImage(){
+    var fileInput1 = document.getElementById('alignSwapUpload1').files;
+    var fileInput2 = document.getElementById('alignSwapUpload2').files;
+    console.log(fileInput1);
+    console.log(fileInput2);
+    if(!fileInput1.length){
+        return alert('Please choose a reference Image to upload first.')
+    }
+    if(!fileInput2.length){
+        return alert('Please choose a Image on which faces needs to be swapped first.')
+    }
+    var file1 = fileInput[0];
+    var filename1 = file1.name;
+    var file2 = fileInput[0];
+    var filename2 = file2.name;
+    var formData = new FormData();
+    formData.append(filename1, file1);
+    formData.append(filename2, file2);
+
+    console.log(filename1);
+    console.log(file1);
+    console.log(filename2);
+    console.log(file2);
+    $.ajax({
+        aync: true,
+        crossDomain: true,
+        method: 'POST',
         url: 'https://xxxxx.execute-api.ap-south-1.amazonaws.com/dev/classify',
         data: formData,
         processData: false,
@@ -86,6 +128,10 @@ function alignImage(){
         alert ("There was error while sending prediction request to mobile model.");
     });
 };
+
+
+
 $('#btnResNetUpload').click(uploadAndClassifyImageRes)
 $('#btnMobileNetUpload').click(uploadAndClassifyImageMobile)
 $('#btnalignUpload').click(alignImage)
+$('#btnswapUpload').click(swapImage)
